7cbe4770401363a11feccc898a775b59761f8a01
Mason Capps <masoncapps42119@gmail.com>
2022-08-15 20:10:23 -0500

    Passes test for running marks

--- bowling-game.rb
+++ bowling-game.rb
@@ -54,5 +54,27 @@ class BowlingGame
   end
 
   def running_marks(pins)
+    score = 0
+    index = 0
+    while index < pins.length
+      if pins[index] == "X"
+        score += 10
+        score += pins[index + 1]
+        if pins[index + 2] == "/"
+          score += 10 - pins[index + 1]
+        else
+          score += pins[index + 2]
+        end
+      elsif pins[index] == "/"
+        score += 10 - pins[index - 1]
+        score += pins[index + 1]
+      elsif pins[index] == "-"
+        score += 0
+      else
+        score += pins[index]
+      end
+      index += 1
+    end
+    return score
   end
 end
